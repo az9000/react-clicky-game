acab9fb5ae59f827a6700df5a94e1df78d63ece6
Fawzi <alami42@live.com>
2019-10-27 10:33:32 -0500

    Updates

---
+++ precache-manifest.a6cfa17993b788266f605a31544b5bd8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eac6fe9114a51b794e55ea5a9da45853",
+    "url": "/react-clicky-game/index.html"
+  },
+  {
+    "revision": "8f6e85dcdf6fe91dee52",
+    "url": "/react-clicky-game/static/css/main.d2a14f57.chunk.css"
+  },
+  {
+    "revision": "7e391e357a60257c48e2",
+    "url": "/react-clicky-game/static/js/2.46d8dda4.chunk.js"
+  },
+  {
+    "revision": "8f6e85dcdf6fe91dee52",
+    "url": "/react-clicky-game/static/js/main.cfaf5ea3.chunk.js"
+  },
+  {
+    "revision": "ee0c537ebdba8deeb5d8",
+    "url": "/react-clicky-game/static/js/runtime-main.e9bdcedb.js"
+  }
+]);
